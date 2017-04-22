3d38c7358490168c55ebea874b8b706edc6ef8d1
Mike Jablonski <mjablonski@gmail.com>
2017-04-22 09:24:58 -0700

    logging updates.

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ var
   actualP = 0,
   pidController;
 
+var dateFormat = require('dateformat');
 var max31855 = require('max31855');
 var thermoSensor = new max31855();
 var actualTemp = 0;
 var WindowSize = 5000;
 var tempHoldTime = 60; // in minutes
 var tempHitTime;
+var tempStopTime;
 
 pidController = new liquidPID({
   temp: {
@@ -23,36 +25,55 @@ pidController = new liquidPID({
 });
 
 var windowStartTime = new Date().getTime();
-var i = 0;
 
 console.log('Target temp:', pidController.getRefTemperature());
 
 function pid() {
   thermoSensor.readTempC(function(temp) {
         actualTemp = temp;
-        console.log('Temp in degrees celsius: %s, in farenheight: %s', temp, temp * 9/5 + 32);
+        //console.log('Temp in degrees celsius: %s, in farenheight: %s', temp, temp * 9/5 + 32);
 
         actualP = pidController.calculate(actualTemp);	// call with the actual temp
-        console.log('Actual p: ', actualP);
+        //console.log('Actual p: ', actualP);
 
         var now = new Date().getTime();
-    
+        var relayStatus = '';
+
+        if (actualTemp >= pidController.getRefTemperature()) {
+          tempHitTime = now;
+          tempStopTime = new Date(tempHitTime + tempHoldTime * 60000);
+        }
+
         if ((now - windowStartTime) > WindowSize) {
             // time to shift the Relay Window
             windowStartTime += WindowSize;
         }
         if (actualP > (now - windowStartTime)) {
             //digitalWrite(RelayPin,HIGH);
-            console.log('Now: %s, Turn Relay ON', now);
+            relayStatus = "ON ";
+            //console.log('Now: %s, Set Relay ON', now);
         }
         else {
             //digitalWrite(RelayPin,LOW);
-            console.log('Now %s, Turn Relay OFF', now);
+            relayStatus = "OFF";
+            //console.log('Now %s, Set Relay OFF', now);
         }
-        
-        i++;
 
-        if (i < 10) {
+        //console.log('Temp Hit Time: %s, Temp Hold Time: %s', tempHitTime, tempHoldTime * 60000);
+
+        console.log('Target:%s, Temp C:%s, Temp F:%s, ActualP:%s, Relay:%s, Temp Hit:%s, Temp Hold:%s min, Now:%s, Stop:%s',
+          Number(pidController.getRefTemperature()).toFixed(2),
+          Number(temp).toFixed(2),
+          Number(temp * 9/5 + 32).toFixed(2),
+          actualP,
+          relayStatus,
+          tempHitTime ? dateFormat(tempHitTime, "hh:MM:ss:L TT") : "TBD",
+          tempHoldTime,
+          dateFormat(now, "hh:MM:ss:L TT"),
+          tempStopTime ? dateFormat(tempStopTime, "hh:MM:ss:L TT"): "TBD");
+
+        // keep calling pid until we hit our temp hold time
+        if (!tempHitTime || (tempHitTime + tempHoldTime * 60000) > now) {
           pid();
         }
     });
