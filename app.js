022d0390e5989953a4e512b19657f1b866fde1e1
Mike Jablonski <mjablonski@gmail.com>
2017-04-23 20:18:27 -0700

    Revert "added untested db logging."
    This reverts commit 1511055e1ccf5ab70d1878f2ebcdc98143246b49.

--- app.js
+++ app.js
@@ -1,49 +1,24 @@
-// usage
-// node app.js <BrewSessionName> <TargetTemp>
-// example:
-// sudo node app.js IPA1 155
+var
+  liquidPID = require('liquid-pid'),
+  actualP = 0,
+  pidController;
 
-var liquidPID = require('liquid-pid'),
-var actualP = 0;
-var pidController;
-
-// read the command line args
-if (process.argv.length < 4) {
-  console.log("Usage: sudo node app.js <BrewSessionName> <TargetTemp>");
-  process.exit();
-}
-var brewSessionName = process.argv[2];  // the brew session name
-var tempHoldTime = process.argv[3];     // time in minutes we hold temp for
-var brewSession;
-
-var loki = require('lokijs');
-var db = new loki('brewSessions.json', 
-      {
-        autoload: true,
-        autoloadCallback : loadHandler,
-        autosave: true, 
-        autosaveInterval: 10000 // 10 seconds
-      });
-
-var dateFormat = require('dateformat');
 var Gpio = require('onoff').Gpio;
 var pinGpioNumHeat = 13;
+var dateFormat = require('dateformat');
 var max31855 = require('max31855');
 var thermoSensor = new max31855();
 var actualTemp = 0;
 var WindowSize = 5000;
-
-var tempHitTime;        // time when we hit the mash temp
-var tempStopTime;       // time when we turn off the heat
-var prevLogTime;        // previous logged timestamp in the pid loop
-
-var logTimeSpan = 5000; // time between log entries in ms
+var tempHoldTime = 5; // in minutes
+var tempHitTime;
+var tempStopTime;
 
 pidController = new liquidPID({
   temp: {
     ref: 23         // Point temperature                                       
   },
-  Pmax: WindowSize, // Max power (output) [Window Size]
+  Pmax: WindowSize,       // Max power (output) [Window Size]
   
   // Tune the PID Controller
   Kp: 25,           // PID: Kp
@@ -53,10 +28,11 @@ pidController = new liquidPID({
 
 var relayHeat = new Gpio(pinGpioNumHeat, 'out'); // uses "GPIO" numbering
 var windowStartTime = new Date().getTime();
-var readVal;            // value read from the probe
-var prevTemp = 0;       // keep track of the previous temp reading in case of errors from the probe
-var hasHitTemp = false; // have we hit our temp yet?
-var relayStatus = '';
+var readVal;
+var prevTemp = 0;
+var hasHitTemp = false;
+
+console.log('Target temp:', pidController.getRefTemperature());
 
 function pid() {
   thermoSensor.readTempC(function(temp) {
@@ -67,19 +43,13 @@ function pid() {
         else {
           prevTemp = actualTemp;
         }
+        //console.log('Temp in degrees celsius: %s, in farenheight: %s', temp, temp * 9/5 + 32);
 
-        // get the "power" value from the pid logic
         actualP = pidController.calculate(actualTemp);	// call with the actual temp
+        //console.log('Actual p: ', actualP);
+
         var now = new Date().getTime();
-        
-        // check if we need to log this temp to the database for this brew session
-        if (!prevLogTime || (now - prevLogTime == logTimeSpan)) {
-          // log this temp in the database
-          brewSession.mashTempData.push(
-            {time: new Date().getTime(), temp: actualTemp}
-          );
-          prevLogTime = now;
-        }
+        var relayStatus = '';
         
         if (!hasHitTemp && (actualTemp >= pidController.getRefTemperature())) {
           hasHitTemp = true;
@@ -93,19 +63,25 @@ function pid() {
         }
         
         if (actualP > (now - windowStartTime)) {
+            //relayHeat.write(0, relayWriteComplete); // 0 is on, 1 is off
             readVal = relayHeat.readSync();
             if (readVal == 1) {
               relayHeat.writeSync(0); // 0 is on, 1 is off
             }
             relayStatus = "ON ";
+            //console.log('Now: %s, Set Relay ON', now);
         }
         else {
+            //relayHeat.write(1, relayWriteComplete); // 0 is on, 1 is off
             readVal = relayHeat.readSync();
             if (readVal == 0) {
               relayHeat.writeSync(1); // 0 is on, 1 is off
             }
             relayStatus = "OFF";
+            //console.log('Now %s, Set Relay OFF', now);
         }
+
+        //console.log('Temp Hit Time: %s, Temp Hold Time: %s', tempHitTime, tempHoldTime * 60000);
 
         console.log('Target:%s, Temp C:%s, Temp F:%s, ActualP:%s, Relay:%s, Temp Hit:%s, Temp Hold:%s min, Now:%s, Stop:%s',
           Number(pidController.getRefTemperature()).toFixed(2),
@@ -122,38 +98,13 @@ function pid() {
         if (!tempHitTime || (tempHitTime + tempHoldTime * 60000) > now) {
           pid();
         }
-        else {
-          cleanUp();
-        }
     });
 }
 
-function cleanUp() {
-  console.log('Cleaning up...');
+function relayWriteComplete(data) {
+  console.log('Write complete: %s', data)
 }
 
-function loadHandler() {
-    // if database did not exist it will be empty so I will intitialize here
-    var coll = db.getCollection('brewSessions');
-    if (coll === null) {
-        coll = db.addCollection('brewSessions');
-    }
-
-    var brewSession = coll.findOne( {'name': brewSessionName} );
-    if (!brewSession) {
-      brewSession = {
-        'name': brewSessionName,
-        'created': new Date().getTime(),
-        'mashStartTime': '',
-        'mashEndTime': '',
-        'mashTemp': tempHoldTime,
-        'mashTempData': [
-          {}
-        ],
-      };
-    }
-
-    // kick off the pid
-    pid();
-}
+// kick off the pid
+pid();
 
